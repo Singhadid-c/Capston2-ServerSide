ab21bf42a00d3add2da78993b18d875fa4900536
iCubeDevTeam <icube.team@hotmail.com>
2021-04-01 01:36:03 +0700

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAPI.DBContext;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        private readonly DatabaseContext _databaseContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _databaseContext.Users.ToList();
+            return Ok(new { result = users, message = "success"});
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            var users = _databaseContext.Add(user);
+            _databaseContext.SaveChanges();
+            return Ok(new { message = "success"});
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser(User user)
+        {
+            var users =  _databaseContext.Update(user);
+            _databaseContext.SaveChanges();
+            return Ok(new { message = "success" });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = _databaseContext.Users.SingleOrDefault(o => o.Id == id);
+            if(user != null)
+            {
+                var users = _databaseContext.Remove(user);
+                _databaseContext.SaveChanges();
+            }
+            return Ok(new { message = "success"});
+        }
+
+
+    }
+}
---
+++ DBContext/DatabaseContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using WebAPI.Models;
+
+#nullable disable
+
+namespace WebAPI.DBContext
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress; Database= Capstone2; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Position)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("position");
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
